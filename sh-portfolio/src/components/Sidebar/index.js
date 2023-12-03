68671ac6061a9b3bef89c0e84174f2af6dddec12
simon harwood <sjwharwood@outlook.com>
2023-12-03 18:45:25 +0000

    Nav bar links and buttons added and styled

--- sh-portfolio/src/components/Sidebar/index.js
+++ sh-portfolio/src/components/Sidebar/index.js
@@ -5,7 +5,9 @@ import { Link, NavLink } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHome} from '@fortawesome/free-solid-svg-icons'
 import { faUser} from '@fortawesome/free-solid-svg-icons'
-import { faEnvelope} from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope, faLinkedin} from '@fortawesome/free-solid-svg-icons'
+import { faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons'
+
 
 
 
@@ -16,7 +18,6 @@ const Sidebar = () => (
       <img className='sub-logo' src={LogoSubtitle} alt="Simon"/>
     </Link>
     <nav>
-
       <NavLink exact="True" activeclassName="active" to ="/">
         <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
       </NavLink>
@@ -28,8 +29,19 @@ const Sidebar = () => (
       <NavLink exact="True" activeclassName="active" className="contact-link"to ="/contact">
         <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
       </NavLink>
-
     </nav>
+      <ul>
+        <li>
+          <a target='_blank' rel='noreferr' href='https://www.linkedin.com/in/simon-harwood-52967b111/'>
+            <FontAwesomeIcon icon={faLinkedinIn} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a target='_blank' rel='noreferr' href='https://github.com/SJWHarwood'>
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
+        </li>
+      </ul>
   </div>
 )
 
